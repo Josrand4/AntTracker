b780a5e48d0e51888b9c1974cb5ce7a845925ea2
Jose-Mtz-Ruiz <eduardo.martinez@uanl.edu.mx>
2026-03-02 19:48:41 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/antracker/MainActivity.java
+++ app/src/main/java/com/example/antracker/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.antracker;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,13 +8,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        // El ID debe ser "main_layout" como en el XML de arriba
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
